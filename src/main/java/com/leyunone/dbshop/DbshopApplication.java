d5cf654cbecd2b107e60c1d6a57b1f08c26c04fb
leyunone <365627310@qq.com>
2022-12-25 17:49:24 +0800

    Initial commit

---
+++ src/main/java/com/leyunone/dbshop/DbshopApplication.java
@@ -0,0 +1,13 @@
+package com.leyunone.dbshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbshopApplication.class, args);
+    }
+
+}
