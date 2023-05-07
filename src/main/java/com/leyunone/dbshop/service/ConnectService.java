1bc0f3b83aa47f776ab06ef13db068ead18b32c3
leyunone <365627310@qq.com>
2023-05-07 23:57:56 +0800

    [ADD] get all table

--- src/main/java/com/leyunone/dbshop/service/ConnectService.java
+++ src/main/java/com/leyunone/dbshop/service/ConnectService.java
@@ -14,6 +14,11 @@ import java.sql.*;
 @Service
 public class ConnectService {
 
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        ConnectService connectService = new ConnectService();
+        connectService.getConnection("","","");
+    }
+
     public void getConnection(String url,String userName,String passWord) throws SQLException, ClassNotFoundException {
         //组装连接信息
         DBInfo dbInfo = DBInfo.builder().url(url).userName(userName).passWord(passWord).build();
@@ -22,38 +27,40 @@ public class ConnectService {
 
         Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/test2023?useUnicode=true&characterEncoding=utf-8&serverTimezone=Asia/Shanghai&allowMultiQueries=true","root","root");
 
-        DatabaseMetaData dbmd=con.getMetaData();
-        System.out.println("数据库已知的用户: " + dbmd.getUserName());
-        System.out.println("数据库的系统函数的逗号分隔列表: " + dbmd.getSystemFunctions());
-        System.out.println("数据库的时间和日期函数的逗号分隔列表: " + dbmd.getTimeDateFunctions());
-        System.out.println("数据库的字符串函数的逗号分隔列表: " + dbmd.getStringFunctions());
-        System.out.println("数据库供应商用于 'schema' 的首选术语: " + dbmd.getSchemaTerm());
-        System.out.println("数据库URL: " + dbmd.getURL());
-        System.out.println("是否允许只读:" + dbmd.isReadOnly());
-        System.out.println("数据库的产品名称:" + dbmd.getDatabaseProductName());
-        System.out.println("数据库的版本:" + dbmd.getDatabaseProductVersion());
-        System.out.println("驱动程序的名称:" + dbmd.getDriverName());
-        System.out.println("驱动程序的版本:" + dbmd.getDriverVersion());
-        System.out.println("数据库中使用的表类型");
-        ResultSet rs = dbmd.getTableTypes();
+        DatabaseMetaData meta = con.getMetaData();
+        ResultSet rs = meta.getTables("blog", null, null,
+                new String[] { "TABLE" });
         while (rs.next()) {
-            System.out.println(rs.getString("TABLE_TYPE"));
+            System.out.println("表名：" + rs.getString(3));
+            System.out.println("表所属用户名：" + rs.getString(2));
+            System.out.println("------------------------------");
+            ResultSet columns = meta.getColumns("blog", null, rs.getString(3), "menu_id");
+            System.out.println(columns);
         }
-        System.out.println("======================================");
-        ResultSet tables = dbmd.getTables(null, null, "%", new String[]{"t_"});
-        while(rs.next()) {
-            System.out.println(tables.getString("TABLE_CAT"));
-            System.out.println(tables.getString("TABLE_SCHEM"));
-            System.out.println(tables.getString("TABLE_NAME"));
-            System.out.println(tables.getString("TABLE_TYPE"));
-            System.out.println(tables.getString("REMARKS"));
-
-        }
+//        this.dbBaseInfo(con);
     }
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        ConnectService connectService = new ConnectService();
-        connectService.getConnection("","","");
+    /**
+     * 数据库基本信息
+     * @param con
+     */
+    private void dbBaseInfo(Connection con) {
+        try {
+            DatabaseMetaData dbmd=con.getMetaData();
+            System.out.println("数据库已知的用户: " + dbmd.getUserName());
+            System.out.println("数据库的系统函数的逗号分隔列表: " + dbmd.getSystemFunctions());
+            System.out.println("数据库的时间和日期函数的逗号分隔列表: " + dbmd.getTimeDateFunctions());
+            System.out.println("数据库的字符串函数的逗号分隔列表: " + dbmd.getStringFunctions());
+            System.out.println("数据库供应商用于 'schema' 的首选术语: " + dbmd.getSchemaTerm());
+            System.out.println("数据库URL: " + dbmd.getURL());
+            System.out.println("是否允许只读:" + dbmd.isReadOnly());
+            System.out.println("数据库的产品名称:" + dbmd.getDatabaseProductName());
+            System.out.println("数据库的版本:" + dbmd.getDatabaseProductVersion());
+            System.out.println("驱动程序的名称:" + dbmd.getDriverName());
+            System.out.println("驱动程序的版本:" + dbmd.getDriverVersion());
+        }catch (Exception e){
+            
+        }
     }
 }
 
